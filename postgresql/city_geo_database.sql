987f7c3f877485c4c821ba2c666363fcb6229433
Danil <dan.ecto.stroy@yandex.ru>
2022-11-17 14:40:18 +0300

    init_db_docker_changes

--- postgresql/city_geo_database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE cities (
-        id UUID NOT NULL,
-        name VARCHAR(65) NOT NULL,
-        latitude DOUBLE PRECISION NOT NULL,
-        longitude DOUBLE PRECISION NOT NULL,
-        PRIMARY KEY (id)
-);
